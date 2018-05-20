bdd68451825645aa644d3da49b03f0ed713268c4
Emil Burman <emil.burman@hotmail.com>
2018-05-20 10:42:24 +0200

    Cleanup of health potion

--- AlchemyBottles/HealthPotion.cs
+++ AlchemyBottles/HealthPotion.cs
@@ -8,9 +8,6 @@ public class HealthPotion : MonoBehaviour
     public float radiusToCheck;                     // The radius to check for targets
     public string tagToTarget;                      // The tag on which to search for targets
     public LayerMask layerToCheck;                  // The layer in to check for targets
-
-    float closestDistanceSqr;                       // The closest distance to the target
-    Vector3 currentPosition;                        // The current position of this object
     bool initialCollision;                          // Checks if this is the first collision
     #endregion
 
@@ -38,12 +35,6 @@ public class HealthPotion : MonoBehaviour
         //Get objects in radius by layer
         Collider[] objects = Physics.OverlapSphere(transform.position, radiusToCheck, layerToCheck);
 
-        // Set max distance
-        closestDistanceSqr = radiusToCheck;
-
-        // Set the current position
-        currentPosition = transform.position;
-
         foreach (Collider target in objects)
         {
             if (target.tag == tagToTarget)
@@ -54,8 +45,6 @@ public class HealthPotion : MonoBehaviour
 
         // After all targets with correct tags are set as hit, remove this game object
         DeactivateGameObject();
-
-
     }
 
     void SetStatus(Collider targetToSetStatus)
@@ -63,6 +52,7 @@ public class HealthPotion : MonoBehaviour
         Debug.Log(targetToSetStatus.tag);
     }
     #endregion
+
     #region Visual management
     void BreakOnImpact()
     {
